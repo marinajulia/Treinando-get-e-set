417590693a6de3ca9e626dbffd53e1609bf3c634
Marina Julia <marinajulia707@gmail.com>
2021-04-27 16:14:08 -0300

    Treinando get e set

---
+++ Exercício6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercício6 {
+    public class Locadora {
+        private string titulo;
+        private int anoLancado;
+
+        public string Titulo {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+
+        public int AnoLancado {
+            get { return anoLancado; }
+            set { anoLancado = value; }
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com o título do filme:");
+            string titulofilme = Console.ReadLine();
+
+            Console.WriteLine("Entre com o ano do filme:");
+            int ano = Convert.ToInt32(Console.ReadLine());
+
+            Locadora filme1 = new Locadora();
+            filme1.Titulo = titulofilme;
+            filme1.AnoLancado = ano;
+
+            Console.WriteLine(filme1.Titulo + " " + filme1.AnoLancado);
+            
+        }
+    }
+}
